14d77d8a0edc76ae44135fc002e956c56954d90f
William Berbet <100874392+will10iam@users.noreply.github.com>
2022-05-09 15:30:25 -0300

    Initial commit

---
+++ models/User.model.js
@@ -0,0 +1,20 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  },
+  passwordHash: { type: String, required: true },
+  role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
+  isActive: { type: Boolean, default: true },
+  disabledOn: { type: Date },
+});
+
+const UserModel = model("User", userSchema);
+
+module.exports = UserModel;
